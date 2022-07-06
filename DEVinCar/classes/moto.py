6aaadfb1c366b4c38578748e94e444df25a4aa30
Breno <hentgenp@gmail.com>
2022-07-06 20:13:23 -0300

    [Update] moto.py

--- DEVinCar/classes/moto.py
+++ DEVinCar/classes/moto.py
@@ -8,12 +8,12 @@ class Moto(Veiculo):
     cpf_comprador, cor):
         super().__init__(data_fabricacao, nome, placa, valor, cpf_comprador, cor)
 
-        idRandom = uuid4()
+        idRandom = str(uuid4())
         self.__chassi = idRandom
         self.__potencia = 170
         self.__rodas = 2
         self.__gasolina = "Gasolina"
-        self.__vendido = False
+        self.vendido = False
     
 
     @property
@@ -33,16 +33,3 @@ class Moto(Veiculo):
     def alterar_info(self):
         cor_nova = str(input(f"Digite a nova cor da moto {self.nome}: "))
         self.cor = cor_nova
-
-    @property
-    def __str__(self):
-        print(f"""
-        Chassi: {self.__chassi}
-        Moto: {self.nome}
-        Placa: {self.placa}
-        Valor: R$ {self.valor:.2f}
-        CPF Comprador: {self.cpf_comprador}
-        Cor: {self.cor}
-        Rodas: {self.__rodas}
-        Potencia (Em Cavalos): {self.__potencia}
-        Tipo Gasolina: {self.__gasolina} """)
