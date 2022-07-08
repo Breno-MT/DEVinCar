a7364ed7ec6fc3460dd0252490983b1e240e7cbb
Breno <hentgenp@gmail.com>
2022-07-08 16:55:54 -0300

    [Update] cls_triciclo.py

--- DEVinCar/classes/cls_triciclo.py
+++ DEVinCar/classes/cls_triciclo.py
@@ -1,5 +1,7 @@
 from classes.classe import Veiculo
 from uuid import uuid4
+from classes.data.data_array import lista_triciclos
+from datetime import datetime
 
 
 class Triciclo(Veiculo):
@@ -31,5 +33,23 @@ class Triciclo(Veiculo):
         cor_nova = str(input(f"Digite a nova cor do triciclo {self.nome}: "))
         self.cor = cor_nova
 
-    def vender_veiculo(self):
-        pass
+    @staticmethod
+    def vender_veiculo():
+        data_compra = datetime.now().strftime("%d/%m/%Y")
+        cpf_cliente = str(input("Antes de começarmos, digite o seu CPF (no max. 11 digitos): "))
+        placa_escolhida = str(input("Digite a placa desejada: "))
+
+        for x in lista_triciclos:
+            
+            if placa_escolhida == x['placa']:
+
+                x['vendido'] = True
+                x['cpf_comprador'] = cpf_cliente
+                return print(f"""
+                ------------ OBRIGADO PELA COMPRA!!! ------------
+                O veículo {x['nome']} foi comprado no valor de R$: {x['valor']:.2f} !
+                Data de compra: {data_compra}
+                """)
+                
+
+        print("A placa não existe ou você não digitou nada. Tente novamente!")
